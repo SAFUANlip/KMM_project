fd01701f256556279fcc3f53ec2fddb06f804966
Arina_Lapteva <lapteva.aa@mipt.ru>
2024-03-14 15:58:33 +0300

    feat: add realisation throw Simulated and BaseMessage

--- src/BaseMessage.py
+++ src/BaseMessage.py
@@ -17,3 +17,9 @@ class BaseMessage(metaclass=ABCMeta):
         self.time = time
         self.sender_ID = sender_ID
         self.receiver_ID = receiver_ID
+
+
+class BaseMessageRadar(BaseMessage):
+    def __init__(self, type: int, priority: int, time: int, sender_ID: int, receiver_ID: int, visible_targets: list):
+        super().__init__(type, priority, time, sender_ID, receiver_ID)
+        self.visible_targets = visible_targets
--- src/classes.py
+++ src/classes.py
@@ -1,15 +1,11 @@
 import numpy as np
-
-messages = {}
-
-
-class Aim:
-    def __init__(self, coordinates):
-        self.coordinates = coordinates  # x, y, z
+from AeroEnv import Airplane
+from Simulated import Simulated
+from BaseMessage import BaseMessageRadar
 
 
-class RadarRound:
-    def __init__(self, coordinates: tuple, pan_start, tilt_start, view_distance: int, pan_per_sec: float, tilt_per_sec: float, pan_cur: float, tilt_cur: float):
+class RadarRound(Simulated):
+    def __init__(self, dispatcher, ID: int, coordinates: tuple, pan_start, tilt_start, view_distance: int, pan_per_sec: float, tilt_per_sec: float, pan_cur: float, tilt_cur: float):
         """ Класс, описывающий работу РЛС кругового типа обзора, является родительским классом
          для РЛС секторного типа обзора:
          :param coordinates: координаты положения РЛС, относительно глобальной СК (x, y, z)
@@ -20,6 +16,8 @@ class RadarRound:
          :param tilt_per_sec: угол раскрыва по углу наклона за секунду обзора
          :param pan_cur: начало секундного сектора обзора в данный момент времени по углу поворота
          :param tilt_cur: начало секундного сектора обзора в данный момент времени по углу наклона"""
+        super().__init__(dispatcher, ID)
+
         self.coordinates = coordinates
         self.pan_start = pan_start
         self.tilt_start = tilt_start
@@ -29,26 +27,27 @@ class RadarRound:
         self.pan_cur = pan_cur
         self.tilt_cur = tilt_cur
 
-    def change_sector_per_sec(self, new_pan_sec: float, new_tilt_sec: float):
-        self.pan_per_sec = new_pan_sec
-        self.tilt_per_sec = new_tilt_sec
-
     def find_aims(self):
-        all_aims = messages['vo']
+        all_aims = self._checkAvailableMessages()
         visible_aims = []
         for aim in all_aims:
-            r = np.linalg.norm(aim.coordinates - self.coordinates)
+            r = np.linalg.norm(aim.pos - self.coordinates)
             if r < self.view_distance:
-                xa, ya, za, = aim.coordinates
+                xa, ya, za, = aim.pos
                 tilt_aim = np.arcsin(za/r) - self.tilt_start
                 pan_aim = np.arcsin(ya/(r*np.cos(tilt_aim))) - self.pan_start
                 if self.pan_cur < pan_aim < self.pan_cur + self.pan_per_sec and self.tilt_cur < tilt_aim < self.tilt_cur + self.tilt_per_sec:
                     visible_aims.append(aim)
         return visible_aims
 
-    def model_step(self):
+    def runSimulationStep(self, time: int):
         visible_aims = self.find_aims()
-        messages['mfr'] = visible_aims
+        msg = BaseMessageRadar(2000, 1, time, self._ID, 1000, visible_aims)
+        self._sendMessage(msg)
+
+    def change_sector_per_sec(self, new_pan_sec: float, new_tilt_sec: float):
+        self.pan_per_sec = new_pan_sec
+        self.tilt_per_sec = new_tilt_sec
 
 
 class RadarSector(RadarRound):
