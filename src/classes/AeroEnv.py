6284d527b4f4c2dc235d2c87a878f2e6cc58c27f
Evdokimov Ilya <evdokimov.ii@phystech.edu>
2024-03-27 12:12:16 +0300

    AeroEnv: explosion method without chain reaction; lifespan of objects.

--- src/classes/AeroEnv.py
+++ src/classes/AeroEnv.py
@@ -1,39 +1,73 @@
 from src.classes.Simulated import Simulated
-from src.classes.Vector import Vector
-
 from src.classes.Movable import Movable
+
+import numpy as np
+import copy
 
 
 class AeroEnv(Simulated):
     def __init__(self, dispatcher, ID: int) -> None:
         super().__init__(dispatcher, ID, None)
+        self.unpacked_entities = []
         self.entities = []
-
+        self.t = 0 # ( ? )
+    
     def getEntities(self) -> list:
         return self.entities
 
     def runSimulationStep(self, t: float = 1) -> None:
-        for entity in self.getEntities():
+        alive_entities = []
+        for entity in self.unpacked_entities:
+            if entity.t_start <= t < entity.t_end:
+                entity.runSimulationStep(t)
+                alive_entities.append(entity)
+        self.entities = alive_entities
 
-            entity.runSimulationStep(t)
+    def addEntity(self, entity) -> None:
+        self.unpacked_entities.append(entity)
 
-    def addEntity(self, entity):
-        self.entities.append(entity)
+    def explosion(self, pos: np.array, expl_rad: float) -> None:
+        updated_entities = []
+        chain_explosion = []
+        for el in self.entities:
+            if not (self.dist(pos, el.pos()) - el.rad < expl_rad):
+                updated_entities.append(el)
+            else:
+                # chain reaction
+                # if el.type_id == missle_type_id:
+                #     chain_explosion.append(el.pos())
+                pass
+        self.entities = updated_entities
+        for pos in chain_explosion:
+            self.explosion(pos, expl_rad)
 
-    def explosion(self, pos, expl_rad):
-        pass
+    def _dist(self, pos1: np.array, pos2: np.array) -> float:
+        return np.linalg.norm(pos1 - pos2)
+
+    def all_trajectories(self):
+        return [t.trajectory for t in self.unpacked_entities_entities]
+
 
 
 class Airplane(Movable):
-    def __init__(self, dispatcher, ID: int, pos: Vector, vel: Vector) -> None:
+    def __init__(self, dispatcher, ID: int, pos: np.array, rad: float, vel: np.array, t_start: float, t_end: float) -> None:
         super().__init__(dispatcher, ID, pos, vel)
         self.vel = vel
         self.type_id = 1
+        self.trajectory = [pos]
+        self.rad = rad
+        self.t_start = t_start
+        self.t_end = t_end
+        self.t = t_start
 
     def runSimulationStep(self, t: int = 1) -> None:
         # print(self.pos, self.vel)
-
         self.pos = self.pos + self.vel
+        self.t += self._simulating_tick
+        self.trajectory.append((self.pos, self.t))
+
+    def pos(self) -> np.array:
+        return copy.deepcopy(self.pos)
 
 
 class Helicopter(Airplane):
